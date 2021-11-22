527cb1000a6ab80538f4a9ca27832aa880f6c0c5
Sina Nazari <sinanazari3000@gmail.com>
2021-11-22 15:53:32 +0330

    Update Or Create Source Code

--- Function-Project/Example.cpp
+++ Function-Project/Example.cpp
@@ -4,39 +4,65 @@
 
 using namespace std;
 
+//Examples For Better Understanding Function
+
 // Sample(1)
 
-// int f(int x)
-// {
-//     return 2 * x;
-// }
+int f(int x)
+{
+    return 2 * x;
+}
 
-// int main()
-// {
-//     int a = 2;
-//     int b;
-//     b = f(a);
-//     cout << b;
+int main()
+{
+    int a = 2;
+    int b;
+    b = f(a);
+    cout << b;
 
-//     getch();
-//     return 0;
-// }
+    getch();
+    return 0;
+}
 
 //Sample(2)
-// int f(int x); //Prototype
 
-// int main()
-// {
-//     int a = 2;
-//     int b;
-//     b = f(a);
-//     cout << b;
+ int f(int x); //Prototype
 
-//     getch();
-//     return 0;
-// }
+int main()
+{
+    int a = 2;
+    int b;
+    b = f(a);
+    cout << b;
 
-// int f(int x)
-// {
-//     return 2 * x;
-// }
+    getch();
+    return 0;
+}
+
+int f(int x)
+{
+    return 2 * x;
+}
+
+//Sample(3)
+
+int f(int a, int b);
+int main()
+{
+    int a, b, z;
+    cout << "Please Enter Number:" << endl;
+    cin >> a;
+    cout << "Please Enter Number:" << endl;
+    cin >> b;
+
+    z = f(a, b);
+    cout << z;
+
+    getch();
+    return 0;
+}
+
+int f(int a, int b)
+{
+    return a + b;
+}
